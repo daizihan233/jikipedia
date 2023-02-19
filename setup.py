880a77d505331c1858f5b318c988adae1f90e90c
HanTools233 <hantools@foxmail.com>
2023-02-19 21:15:31 +0800

    新增：我不好说

--- setup.py
+++ setup.py
@@ -17,5 +17,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['requests', 'pycryptodome', 'uuid']
+    install_requires=['requests']
 )
